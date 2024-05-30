0e935cd5ad53a5d5f470fc84546237fa780bea24
Debashis Roy <goodquote39@hotmail.com>
2024-05-30 14:59:35 +0600

    rtk practice with react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AllProducts from "./Components/AllProducts";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <AllProducts />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { productsApi } from "./service/Data";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+setupListeners(store.dispatch);
