1c4875e49f78d73c2a3ba0c13407df78684f5fca
Debashis Roy <goodquote39@hotmail.com>
2024-05-30 16:24:44 +0600

    Added useGetProductsQuery hook to AllProducts component and updated rendering logic to handle loading, error, and data states.

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,7 +1,26 @@
+import { useGetProductsQuery } from "../Redux/service/Data";
+
 const AllProducts = () => {
+  const res = useGetProductsQuery();
+  const { data, error, isLoading } = res;
+  //   console.log(data, error, isLoading);
+
   return (
     <div>
-      <p>All products</p>
+      {isLoading ? (
+        <h5>Loading...</h5>
+      ) : error ? (
+        <h5>Error: {error.message}</h5>
+      ) : (
+        <>
+          <h5>Found products {data.products.length}</h5>
+          <ul style={{ width: "500px", textAlign: "left", margin: "auto" }}>
+            {data.products.map((product) => (
+              <li key={product.id}>{product.title}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
